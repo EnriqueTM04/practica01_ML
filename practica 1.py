f169e02ab1bc40cc3ad061d0043c8b73aee0f6a9
EnriqueTM04 <enriquetabo03@gmail.com>
2025-02-13 17:28:25 -0600

    first commit

---
+++ practica 1.py
@@ -0,0 +1,18 @@
+"""Integrantes del eqipo:
+    - ...
+    - ...
+    - Taboada Montiel Enrique
+    
+    Practica 1: Akinator
+    
+"""
+
+respuesta = input("Estas pensando en algo real o ficticio? ")
+if respuesta == "real":
+    respuesta = input("Estas pensando en un animal? ")
+    if respuesta == "si":
+        respuesta = input("Tu animal tiene 4 patas? ")
+        if respuesta == "si":
+            respuesta = input("Tu animal es un mamifero? ")
+            if respuesta == "si":
+                respuesta = input("Tu animal es un terrestre? ")
