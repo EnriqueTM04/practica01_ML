12905c15c4610d34809f66c27f5c77037c0c0847
EnriqueTM04 <enriquetabo03@gmail.com>
2025-02-13 17:29:47 -0600

    Segundo commit

--- practica 1.py
+++ practica 1.py
@@ -7,8 +7,8 @@
     
 """
 
-respuesta = input("Estas pensando en algo real o ficticio? ")
-if respuesta == "real":
+respuesta = input("Estas pensando en algo real? ")
+if respuesta == "si":
     respuesta = input("Estas pensando en un animal? ")
     if respuesta == "si":
         respuesta = input("Tu animal tiene 4 patas? ")
